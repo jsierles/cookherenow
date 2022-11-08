a28d5b7807c06b60986722fa29370f75fff76918
Joshua Sierles <joshua@fly.io>
2022-11-08 17:45:25 +0100

    cache query results in Redis

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,15 @@ class HomeController < ApplicationController
   def home
     ip = $geoip.city(request.headers['Fly-Client-Ip']) rescue nil
 
-    @recipes = if params[:q]
-      Recipe.where("lower(data->>'title') LIKE '%#{params[:q].downcase}%'")
-    elsif ip
-      @appear = ip.city.name || ip.country.name
-      Recipe.where("lower(data->>'title') LIKE '%#{ip.city.name.downcase}%' OR lower(data->>'title') LIKE '%#{ip.country.name.downcase}%'")
-    else
-      Recipe.none
+    @recipes = Rails.cache.fetch(params[:q]) do
+      if params[:q]
+        Recipe.where("lower(data->>'title') LIKE '%#{params[:q].downcase}%'")
+      elsif ip
+        @appear = ip.city.name || ip.country.name
+        Recipe.where("lower(data->>'title') LIKE '%#{ip.city.name.downcase}%' OR lower(data->>'title') LIKE '%#{ip.country.name.downcase}%'")
+      else
+        Recipe.none
+      end
     end
   end
 end
