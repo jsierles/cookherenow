5cbfa62ae324b74b2f7472b45b03da98a0544ffa
Joshua Sierles <joshua@fly.io>
2022-11-08 22:08:14 +0100

    cache views instead

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,17 @@
 class HomeController < ApplicationController
   def home
     ip = $geoip.city(request.headers['Fly-Client-Ip']) rescue nil
+    @key = nil
 
-    @recipes = Rails.cache.fetch(params[:q], expires_in: 10.seconds) do
-      if params[:q]
-        Recipe.where("lower(data->>'title') LIKE '%#{params[:q].downcase}%'")
-      elsif ip
-        @appear = ip.city.name || ip.country.name
-        Recipe.where("lower(data->>'title') LIKE '%#{ip.city.name.downcase}%' OR lower(data->>'title') LIKE '%#{ip.country.name.downcase}%'")
-      else
-        Recipe.none
-      end
+    @recipes = if params[:q]
+      Recipe.where("lower(data->>'title') LIKE '%#{params[:q].downcase}%'")
+      key = params[:q]
+    elsif ip
+      @appear = ip.city.name || ip.country.name
+      Recipe.where("lower(data->>'title') LIKE '%#{ip.city.name.downcase}%' OR lower(data->>'title') LIKE '%#{ip.country.name.downcase}%'")
+      key = ip
+    else
+      Recipe.none
     end
   end
 end
