03d141579b34ba2bf714209a411d624557789d4a
Joshua Sierles <joshua@fly.io>
2022-11-17 05:36:54 +0100

    don't need the edamame client

--- config/initializers/edamam.rb
+++ config/initializers/edamam.rb
@@ -1,1 +1,1 @@
-$edamam = Edamam::Client.new(app_id: "9cd5f5de", app_key: Rails.application.credentials.dig(:edamam_key))
+# $edamam = Edamam::Client.new(app_id: "9cd5f5de", app_key: Rails.application.credentials.dig(:edamam_key))
