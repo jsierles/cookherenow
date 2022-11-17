081949864a09f7941925042e33c4042f1a66f331
Joshua Sierles <joshua@fly.io>
2022-11-17 05:21:17 +0100

    updates

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,17 +1,18 @@
-class HomeController < ApplicationController
-  def home
+class RecipesController < ApplicationController
+  def index
     ip = $geoip.city(request.headers['Fly-Client-Ip']) rescue nil
-    @key = nil
 
     @recipes = if params[:q]
-      @key = params[:q]
       Recipe.where("lower(data->>'title') LIKE '%#{params[:q].downcase}%'")
     elsif ip
       @appear = ip.city.name || ip.country.name
-      @key = ip
       Recipe.where("lower(data->>'title') LIKE '%#{ip.city.name.downcase}%' OR lower(data->>'title') LIKE '%#{ip.country.name.downcase}%'")
     else
       Recipe.none
     end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#home"
+  root to: "recipes#index"
+
+  resources :recipes
   get '/rack-headers' => 'rack_headers#show'
 end
